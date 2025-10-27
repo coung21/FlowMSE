3197363369c22adaa5ec29b4681d2e701ab88ff3
coung21 <coungdev@gmail.com>
2025-10-27 16:15:46 +0700

    Remove exclamation marks from gdown and unzip commands in download_mixed.sh

--- data/download_mixed.sh
+++ data/download_mixed.sh
@@ -1,2 +1,2 @@
-!gdown 1SGOBZXoi2SaKcWAjAfrn7g60ol8dF7-u -O ./data/dataset.zip
-!unzip -o ./data/dataset.zip -d ./data/
+gdown 1SGOBZXoi2SaKcWAjAfrn7g60ol8dF7-u -O ./data/dataset.zip
+unzip -o ./data/dataset.zip -d ./data/
